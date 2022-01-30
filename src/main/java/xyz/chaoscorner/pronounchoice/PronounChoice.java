5664a97ec208bc6797e74536b77a80d9a4fceffa
Michael Bailey <mpabailey@gmail.com>
2022-01-30 17:52:04 -0500

    Init commit

---
+++ src/main/java/xyz/chaoscorner/pronounchoice/PronounChoice.java
@@ -0,0 +1,17 @@
+package xyz.chaoscorner.pronounchoice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PronounChoice extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
