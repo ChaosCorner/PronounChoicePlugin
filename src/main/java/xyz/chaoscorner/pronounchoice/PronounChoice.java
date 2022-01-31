56fc4a4336281f451a64a6f545c8f32ba64fbb34
Michael Bailey <mpabailey@gmail.com>
2022-01-30 21:45:07 -0500

    Add connection to LuckPerms to main starter

--- src/main/java/xyz/chaoscorner/pronounchoice/PronounChoice.java
+++ src/main/java/xyz/chaoscorner/pronounchoice/PronounChoice.java
@@ -1,13 +1,30 @@
 package xyz.chaoscorner.pronounchoice;
 
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import xyz.chaoscorner.pronounchoice.commands.PronounCommandManager;
+import xyz.chaoscorner.pronounchoice.util.LuckPermsDataAccess;
 
 public final class PronounChoice extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // connect to luckperms
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider == null) {
+            getLogger().severe("Cannot load LuckPerms provider. Is LuckPerms working?");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 
+        LuckPerms luckPerms = provider.getProvider();
+
+        // create the data access lib
+        LuckPermsDataAccess luckPermsDA = new LuckPermsDataAccess(luckPerms);
+
+        // initialize the main pronoun command
+        getCommand("pronoun").setExecutor(new PronounCommandManager(luckPermsDA));
     }
 
     @Override
